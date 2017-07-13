b61091d8ea33d22da7a8ccae1a26099f8041dc69
Carlos Sanchez <papachoco@gmail.com>
2017-07-13 18:15:14 -0500

    resolve to content package and fix call

--- src/nti/app/contentlibrary_store/roles.py
+++ src/nti/app/contentlibrary_store/roles.py
@@ -13,47 +13,22 @@ import six
 
 from zope import component
 
-from nti.contentlibrary.interfaces import IContentPackageLibrary
+from nti.app.contentlibrary.utils import role_for_content_bundle
+from nti.app.contentlibrary.utils import role_for_content_package
+
+from nti.contentlibrary.interfaces import IContentUnit
+from nti.contentlibrary.interfaces import IContentPackage
+from nti.contentlibrary.interfaces import IContentPackageBundle
 
 from nti.dataserver.authorization import CONTENT_ROLE_PREFIX
-from nti.dataserver.authorization import role_for_providers_content
 
 from nti.dataserver.interfaces import IMutableGroupMember
 
-from nti.ntiids.ntiids import get_provider
-from nti.ntiids.ntiids import get_specific
-from nti.ntiids.ntiids import is_valid_ntiid_string
+from nti.ntiids.ntiids import find_object_with_ntiid
 
 from nti.store import get_user
 
-
-def get_library(library=None):
-    if library is None:
-        return component.queryUtility(IContentPackageLibrary)
-    return library
-
-
-def get_paths(ntiid, library=None):
-    library = get_library(library)
-    paths = library.pathToNTIID(ntiid) if library and ntiid else ()
-    return paths or ()
-
-
-def get_ntiid_path(ntiid, library=None):
-    result = get_paths(ntiid, library)
-    result = tuple(p.ntiid for p in result) if result else ()
-    return result
-
-
-def get_collection_root(ntiid, library=None):
-    paths = get_paths(ntiid, library)
-    return paths[0] if paths else None
-
-
-def get_collection_root_ntiid(ntiid, library=None):
-    root = get_collection_root(ntiid, library)
-    result = root.ntiid if root is not None else None
-    return result
+from nti.traversal.traversal import find_interface
 
 
 def get_descendants(unit):
@@ -67,21 +42,22 @@ def get_descendants(unit):
             return
 
 
-def check_item_in_library(item, library=None):
-    result = None
-    if item and is_valid_ntiid_string(item):
-        result = get_collection_root_ntiid(item, library=library)
+def resolve(ntiid):
+    result = find_object_with_ntiid(ntiid)
+    if IContentUnit.providedBy(result):
+        result = find_interface(result, IContentPackage, strict=False)
+    elif not IContentPackageBundle.providedBy(result):
+        result = None
     return result
 
 
-def get_role_for_item(item):
-    provider = get_provider(item).lower()
-    specific = get_specific(item).lower()
-    role = role_for_providers_content(provider, specific)
-    return role
+def get_role(context):
+    if IContentPackageBundle.providedBy(context):
+        return role_for_content_bundle(context)
+    return role_for_content_package(context)
 
 
-def add_users_content_roles(user, items, library=None):
+def add_users_content_roles(user, items):
     """
     Add the content roles to the given user
 
@@ -96,24 +72,25 @@ def add_users_content_roles(user, items, library=None):
     if user is None or not items:
         return 0
 
-    roles_to_add = set()
+    # parse current roles
+    roles_to_add = []
     member = component.getAdapter(user,
                                   IMutableGroupMember,
                                   CONTENT_ROLE_PREFIX)
     current_roles = {x.id: x for x in member.groups}
-
+    # resolve new roles
     for item in items:
-        lib_item = check_item_in_library(item, library)
-        if lib_item is None:
-            logger.debug("Ignoring %s item", item)
+        source = resolve(item)
+        if source is None:
             continue
-
-        role = get_role_for_item(lib_item)
+        role = get_role(source)
         if role.id not in current_roles:
-            logger.info("Role %s added to %s", role.id, user)
-            roles_to_add.add(role)
-
-    member.setGroups(list(current_roles.values()) + list(roles_to_add))
+            logger.debug("Role %s added to %s", role.id, user)
+            roles_to_add.append(role)
+    # add new roles
+    if roles_to_add:
+        current_roles = list(current_roles.values())
+        member.setGroups(current_roles + roles_to_add)
     return len(roles_to_add)
 
 
@@ -124,33 +101,34 @@ def remove_users_content_roles(user, items, library=None):
     :param user: The user object
     :param items: List of ntiids
     """
+    if isinstance(items, six.string_types):
+        items = set(items.split())
+
     user = get_user(user)
     if not user or not items:
         return 0
-
     member = component.getAdapter(user,
                                   IMutableGroupMember,
                                   CONTENT_ROLE_PREFIX)
     if not member.hasGroups():
         return 0
 
-    roles_to_remove = set()
+    # parse current roles
+    roles_to_remove = []
     current_roles = {x.id.lower(): x for x in member.groups}
     current_size = len(current_roles)
-
     for item in items:
-        item = check_item_in_library(item, library)
-        if item is not None:
-            provider = get_provider(item).lower()
-            specific = get_specific(item).lower()
-            role = role_for_providers_content(provider, specific)
-            roles_to_remove.add(role.id)
-
+        source = resolve(item)
+        if source is None:
+            continue
+        role = get_role(source)
+        roles_to_remove.append(role.id)
+    # remove from current roles
     for r in roles_to_remove:
-        if current_roles.pop(r, None):
-            logger.info("Role %s removed from %s", r, user)
-
-    member.setGroups(list(current_roles.values()))
+        current_roles.pop(r, None)
+    # set new roles
+    current_roles = list(current_roles.values())
+    member.setGroups(current_roles)
     return current_size - len(current_roles)
 
 
@@ -164,37 +142,10 @@ def get_users_content_roles(user):
     member = component.getAdapter(user,
                                   IMutableGroupMember,
                                   CONTENT_ROLE_PREFIX)
-
     result = []
     for x in member.groups or ():
         if x.id.startswith(CONTENT_ROLE_PREFIX):
             spl = x.id[len(CONTENT_ROLE_PREFIX):].split(':')
             if len(spl) >= 2:
                 result.append((spl[0], spl[1]))
-    return result
-
-
-def get_user_accessible_content(user, library=None):
-    user = get_user(user)
-    member = component.getAdapter(user,
-                                  IMutableGroupMember,
-                                  CONTENT_ROLE_PREFIX)
-
-    if library is None:
-        library = component.queryUtility(IContentPackageLibrary)
-
-    packages = {}
-    for package in (library.contentPackages if library is not None else ()):
-        provider = get_provider(package.ntiid).lower()
-        specific = get_specific(package.ntiid).lower()
-        role = role_for_providers_content(provider, specific)
-        packages[role.id] = package.ntiid
-
-    result = set()
-    for x in member.groups or ():
-        ntiid = packages.get(x.id, None)
-        if ntiid:
-            ntiid = get_collection_root_ntiid(ntiid, library=library)
-            result.add(ntiid)
-    result.discard(None)
     return result
