6949c6a06c3c51542881752b3b842d97db6cb360
Carlos Sanchez <papachoco@gmail.com>
2017-07-13 18:18:55 -0500

    fix test

--- src/nti/app/contentlibrary_store/tests/test_subscribers.py
+++ src/nti/app/contentlibrary_store/tests/test_subscribers.py
@@ -13,7 +13,6 @@ from hamcrest import is_not
 from hamcrest import assert_that
 
 import os
-import unittest
 
 from zope import component
 
@@ -25,8 +24,6 @@ from nti.app.contentlibrary_store.roles import get_users_content_roles
 from nti.contentlibrary.interfaces import IFilesystemContentPackageLibrary
 
 from nti.contentlibrary.filesystem import DynamicFilesystemLibrary as FileLibrary
-
-from nti.dataserver.users import User
 
 from nti.invitations.interfaces import IInvitationsContainer
 
@@ -46,14 +43,14 @@ from nti.store.store import get_gift_code
 from nti.app.contentlibrary_store.tests import create_and_register_gift_attempt
 from nti.app.contentlibrary_store.tests import create_and_register_purchase_attempt
 
-from nti.app.contentlibrary_store.tests import SharedConfiguringTestLayer
+from nti.app.testing.application_webtest import ApplicationLayerTest
 
-from nti.dataserver.tests.mock_dataserver import WithMockDSTrans
+from nti.app.testing.decorators import WithSharedApplicationMockDS
+
+from nti.dataserver.tests import mock_dataserver
 
 
-class TestSubscribers(unittest.TestCase):
-
-    layer = SharedConfiguringTestLayer
+class TestSubscribers(ApplicationLayerTest):
 
     ntiid = u"tag:nextthought.com,2011-10:MN-purchasable_content-MiladyCosmetology.cosmetology"
 
@@ -64,58 +61,61 @@ class TestSubscribers(unittest.TestCase):
         component.provideUtility(library, IFilesystemContentPackageLibrary)
 
     def create_user(self, username=u'ichigo@bleach.org', password=u'temp001'):
-        usr = User.create_user(self.ds, username=username, password=password)
-        return usr
+        return self._create_user(username, password)
 
-    @WithMockDSTrans
+    @WithSharedApplicationMockDS(testapp=True, users=True)
     def test_success(self):
-        user = self.create_user()
-        purchase = create_and_register_purchase_attempt(user, self.ntiid)
-        notify(PurchaseAttemptSuccessful(purchase))
-        roles = get_users_content_roles(user)
-        assert_that(roles, is_([('mn', 'miladycosmetology.cosmetology')]))
+        with mock_dataserver.mock_db_trans(self.ds):
+            user = self.create_user()
+            purchase = create_and_register_purchase_attempt(user, self.ntiid)
+            notify(PurchaseAttemptSuccessful(purchase))
+            roles = get_users_content_roles(user)
+            assert_that(roles, is_([('mn', 'miladycosmetology.cosmetology')]))
 
-    @WithMockDSTrans
+    @WithSharedApplicationMockDS(testapp=True, users=True)
     def test_refund_simple_purchase(self):
-        user = self.create_user()
-        add_users_content_roles(user, (self.ntiid,))
-        purchase = create_and_register_purchase_attempt(user, self.ntiid)
-        purchase.State = PA_STATE_SUCCESS
-        notify(PurchaseAttemptRefunded(purchase))
-        roles = get_users_content_roles(user)
-        assert_that(roles, is_([]))
+        with mock_dataserver.mock_db_trans(self.ds):
+            user = self.create_user()
+            add_users_content_roles(user, (self.ntiid,))
+            purchase = create_and_register_purchase_attempt(user, self.ntiid)
+            purchase.State = PA_STATE_SUCCESS
+            notify(PurchaseAttemptRefunded(purchase))
+            roles = get_users_content_roles(user)
+            assert_that(roles, is_([]))
 
-    @WithMockDSTrans
+    @WithSharedApplicationMockDS(testapp=True, users=True)
     def test_refund_invitation_purchase(self):
-        user = self.create_user()
-        add_users_content_roles(user, (self.ntiid,))
-        # create invitation purchase attempt
-        purchase = create_and_register_purchase_attempt(user, self.ntiid, 5)
-        # accept invitation
-        invite = create_store_purchase_invitation(purchase, receiver=user)
-        invitations = component.getUtility(IInvitationsContainer)
-        invitations.add(invite)
-        accept_invitation(user, invite)
-        # refund
-        notify(PurchaseAttemptRefunded(purchase))
-        roles = get_users_content_roles(user)
-        assert_that(roles, is_([]))
+        with mock_dataserver.mock_db_trans(self.ds):
+            user = self.create_user()
+            add_users_content_roles(user, (self.ntiid,))
+            # create invitation purchase attempt
+            purchase = create_and_register_purchase_attempt(user, self.ntiid, 5)
+            # accept invitation
+            invite = create_store_purchase_invitation(purchase, receiver=user)
+            invitations = component.getUtility(IInvitationsContainer)
+            invitations.add(invite)
+            accept_invitation(user, invite)
+            # refund
+            notify(PurchaseAttemptRefunded(purchase))
+            roles = get_users_content_roles(user)
+            assert_that(roles, is_([]))
 
-    @WithMockDSTrans
+    @WithSharedApplicationMockDS(testapp=True, users=True)
     def test_refund_gift_purchase(self):
-        ichigo = self.create_user()
-        aizen = self.create_user(u'aizen@bleach.org',)
-        # create gift purchase attempt
-        purchase = create_and_register_gift_attempt(ichigo.username,
-                                                    aizen.username, 
-                                                    self.ntiid)
-        code = get_gift_code(purchase)
-        # accept gift
-        notify(GiftPurchaseAttemptRedeemed(purchase, aizen, code=code))
-        # refund
-        target = purchase.TargetPurchaseID
-        attempt = get_purchase_attempt(target)
-        assert_that(attempt, is_not(none()))
-        notify(PurchaseAttemptRefunded(attempt))
-        roles = get_users_content_roles(aizen)
-        assert_that(roles, is_([]))
+        with mock_dataserver.mock_db_trans(self.ds):
+            ichigo = self.create_user()
+            aizen = self.create_user(u'aizen@bleach.org',)
+            # create gift purchase attempt
+            purchase = create_and_register_gift_attempt(ichigo.username,
+                                                        aizen.username, 
+                                                        self.ntiid)
+            code = get_gift_code(purchase)
+            # accept gift
+            notify(GiftPurchaseAttemptRedeemed(purchase, aizen, code=code))
+            # refund
+            target = purchase.TargetPurchaseID
+            attempt = get_purchase_attempt(target)
+            assert_that(attempt, is_not(none()))
+            notify(PurchaseAttemptRefunded(attempt))
+            roles = get_users_content_roles(aizen)
+            assert_that(roles, is_([]))
